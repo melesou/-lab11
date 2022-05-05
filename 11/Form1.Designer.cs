0d2aff0d5d7bd7429607394acdf78b91ff314178
melesou <melesou@mail.ru>
2022-05-05 15:50:21 +0500

    Добавьте файлы проекта.

---
+++ 11/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label17 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label17
+            // 
+            this.label17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(163)))), ((int)(((byte)(13)))));
+            this.label17.Location = new System.Drawing.Point(10, 10);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(780, 430);
+            this.label17.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(60)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label17);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label17;
+    }
+}
+
---
+++ 11/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.FromArgb(31, 58, 96));
+            Random rand = new Random();
+            SolidBrush zxc = new SolidBrush(Color.FromArgb(31, 58, 96));
+
+            SolidBrush whit = new SolidBrush(Color.White);
+            SolidBrush yell = new SolidBrush(Color.Yellow);
+            SolidBrush awhit = new SolidBrush(Color.AntiqueWhite);
+            SolidBrush gray = new SolidBrush(Color.Gray);
+            SolidBrush bGray = new SolidBrush(Color.DarkGray);
+            SolidBrush sky = new SolidBrush(Color.SkyBlue);
+            SolidBrush bGreeneer = new SolidBrush(Color.FromArgb(48, 80, 67));
+            SolidBrush bGreener = new SolidBrush(Color.FromArgb(50, 93, 69));
+            SolidBrush bGreen = new SolidBrush(Color.FromArgb(51, 100, 71));
+            SolidBrush brown = new SolidBrush(Color.FromArgb(61, 56, 54));
+            SolidBrush lightBrown = new SolidBrush(Color.FromArgb(85, 72, 72));
+            SolidBrush black = new SolidBrush(Color.Black);
+
+
+            //Земля
+            g.FillRectangle(bGreeneer, 0, 310, 900, 110);
+            g.FillRectangle(bGreener, 0, 330, 900, 110);
+
+            //Звезды
+            int size = 0;
+            int count = 0;
+            for (int i = 0; i < 1000; i += 10)
+            {
+                size = rand.Next(2, 5);
+                count = rand.Next(1, 5);
+                if (count == 1)
+                    g.FillRectangle(yell, rand.Next(i, i + 50), rand.Next(0, 300), size, size);
+                if (count == 2)
+                    g.FillRectangle(whit, rand.Next(i, i + 50), rand.Next(0, 300), size, size);
+                if (count == 3)
+                    g.FillRectangle(sky, rand.Next(i, i + 50), rand.Next(0, 300), size, size);
+                if (count == 4)
+                    g.FillRectangle(whit, rand.Next(i, i + 50), rand.Next(0, 300), size, size);
+            }
+            //Луна
+            g.FillEllipse(awhit, 350, 10, 80, 80);
+            g.FillEllipse(gray, 385, 20, 10, 10);
+            g.FillEllipse(gray, 400, 30, 12, 12);
+            g.FillEllipse(gray, 390, 50, 8, 8);
+            g.FillEllipse(gray, 403, 60, 15, 15);
+            g.FillRectangle(zxc, 310, 10, 80, 80);
+
+            //Замок
+            g.FillRectangle(gray, 600, 275, 60, 60);
+            g.FillRectangle(brown, 615, 305, 20, 30);
+            g.FillRectangle(lightBrown, 618, 335, 15, 40);
+
+
+            g.FillRectangle(bGray, 560, 250, 40, 90);
+            g.FillRectangle(gray, 555, 240, 50, 20);
+            g.FillRectangle(gray, 575, 280, 10, 18);
+            g.FillRectangle(black, 575, 280, 10, 15);
+
+            g.FillRectangle(bGray, 650, 250, 40, 90);
+            g.FillRectangle(gray, 645, 240, 50, 20);
+            g.FillRectangle(gray, 665, 280, 10, 18);
+            g.FillRectangle(black, 665, 280, 10, 15);
+
+            // Земля
+            g.FillRectangle(bGreen, 0, 350, 900, 130);
+            g.FillRectangle(lightBrown, 540, 350, 70, 130);
+        }
+    }
+}
